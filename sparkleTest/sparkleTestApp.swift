1e7b28a66506debd0c9f1a1b1aafad66e8bd2a83
Rockmed1 <186123210+Rockmed1@users.noreply.github.com>
2026-05-19 17:51:39 -0700

    Initial Commit

---
+++ sparkleTest/sparkleTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  sparkleTestApp.swift
+//  sparkleTest
+//
+//  Created by ahmd on 5/19/26.
+//
+
+import SwiftUI
+
+@main
+struct sparkleTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sparkleTestTests/sparkleTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  sparkleTestTests.swift
+//  sparkleTestTests
+//
+//  Created by ahmd on 5/19/26.
+//
+
+import Testing
+@testable import sparkleTest
+
+struct sparkleTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
